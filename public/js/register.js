4e70f0869655d2d11b96a2c9dae969a8000c0fd7
serj <srdjan@adjust.com>
2019-09-19 23:25:25 +0200

    register - enter username & passwrod

--- public/js/register.js
+++ public/js/register.js
@@ -1,31 +1,24 @@
 function registerPost() {
-    // $.ajax({
-    //     type: 'POST',
-    //     url: '/users',
-    //     contentType: "application/json; charset=utf-8",
-    //     dataType: 'json',
-    //     data: JSON.stringify({ username: '2beens', password: 'dummy' }),
-    //     success: function (data, textStatus, jQxhr) {
-    //         console.log('reply: ' + data);
-    //     },
-    //     error: function (jqXhr, textStatus, errorThrown) {
-    //         console.error(errorThrown);
-    //     }
-    // });
+    var username = $('#username').val();
+    var password = $('#password').val();
+    if (!username || !password) {
+        console.error('username | password empty');
+        return;
+    }
 
     $.ajax({
         url: "/users",
         type: "POST",
         dataType: "json",                 // expected format for response
         contentType: "application/x-www-form-urlencoded; charset=utf-8",  // send as JSON
-        data: { username: "John", password: "Boston" },
-        complete: function() {
+        data: {username: username, password: password},
+        complete: function () {
             console.log('completed');
         },
-        success: function(data, textStatus, jQxhr) {
+        success: function (data, textStatus, jQxhr) {
             console.log('response: ' + JSON.stringify(data));
         },
-        error: function(jqXhr, textStatus, errorThrown) {
+        error: function (jqXhr, textStatus, errorThrown) {
             console.log('response: ' + JSON.stringify(data));
         },
     });
