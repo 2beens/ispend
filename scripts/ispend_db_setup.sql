e3a21c6f8cb6b955b70b69a2a33090c9f505fc9a
serj <srdjan@adjust.com>
2019-09-26 19:00:11 +0200

    db setup script v04

--- scripts/ispend_db_setup.sql
+++ scripts/ispend_db_setup.sql
@@ -1,3 +1,5 @@
+SET TIME ZONE '+00:00';
+
 DROP TABLE IF EXISTS spends;
 DROP TABLE IF EXISTS spend_kinds;
 DROP TABLE IF EXISTS users;
@@ -25,8 +27,8 @@ CREATE TABLE default_spend_kinds (
 CREATE TABLE spends (
     id serial PRIMARY KEY,
     currency char(10) NOT NULL,
-    amount integer NOT NULL,
-    timestamp timestamp NOT NULL, /*DEFAULT CURRENT_TIMESTAMP,*/
+    amount real NOT NULL,
+    spend_timestamp timestamp NOT NULL, /*DEFAULT CURRENT_TIMESTAMP,*/
     user_id integer NOT NULL,
     kind_id integer NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
@@ -40,3 +42,11 @@ INSERT INTO default_spend_kinds (name) VALUES ('Food');
 
 INSERT INTO users (email, username, password) VALUES ('admin@serjspends.de', 'admin', 'admin1');
 INSERT INTO users (email, username, password) VALUES ('lazar@serjspends.de', 'lazar', 'lazar1');
+
+INSERT INTO spend_kinds (user_id, name) VALUES (1, 'House');
+INSERT INTO spend_kinds (user_id, name) VALUES (1, 'Car');
+INSERT INTO spend_kinds (user_id, name) VALUES (2, 'House');
+INSERT INTO spend_kinds (user_id, name) VALUES (2, 'Car');
+
+INSERT INTO spends (currency, amount, spend_timestamp, user_id, kind_id) VALUES ('EUR', 1000, '2019-02-02 00:00:01', 1, 2);
+INSERT INTO spends (currency, amount, spend_timestamp, user_id, kind_id) VALUES ('RSD', 120.60, '2019-01-01 00:00:01', 2, 2);
